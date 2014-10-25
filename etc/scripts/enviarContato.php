d5bb39d14f4ea24f46370c9b62d89835b0590066
Lucas Gomide <lgomide@uplexis.com.br>
2014-10-25 13:37:04 -0200

    fixing conflict

--- etc/scripts/enviarContato.php
+++ etc/scripts/enviarContato.php
@@ -3,68 +3,7 @@
  * This example shows settings to use when sending via Google's Gmail servers.
  */
 
-<<<<<<< HEAD
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
 
-require '../plugin/PhpMailer/PHPMailerAutoload.php';
-
-$mail = new PHPMailer();
-
-// Define os dados do servidor e tipo de conexão
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsSMTP(); // Define que a mensagem será SMTP
-$mail->Host = "mail.axiomteam.works"; // Endereço do servidor SMTP (caso queira utilizar a autenticação, utilize o host smtp.seudomínio.com.br)
-$mail->SMTPAuth = true; // Usar autenticação SMTP (obrigatório para smtp.seudomínio.com.br)
-$mail->Username = 'say@axiomteam.works'; // Usuário do servidor SMTP (endereço de email)
-$mail->Password = '#@ax1omte@m'; // Senha do servidor SMTP (senha do email usado)
-
-// Define o remetente
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->From = "say@axiomteam.works"; // Seu e-mail
-$mail->Sender = "say@axiomteam.works"; // Seu e-mail
-$mail->FromName = "Marco Teste"; // Seu nome
-
-// Define os destinatário(s)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->AddAddress('say@axiomteam.works', 'Teste Marco');
-//$mail->AddAddress('e-mail@destino2.com.br');
-//$mail->AddCC('ciclano@site.net', 'Ciclano'); // Copia
-//$mail->AddBCC('fulano@dominio.com.br', 'Fulano da Silva'); // Cópia Oculta
-
-// Define os dados técnicos da Mensagem
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->IsHTML(true); // Define que o e-mail será enviado como HTML
-//$mail->CharSet = 'iso-8859-1'; // Charset da mensagem (opcional)
-
-// Define a mensagem (Texto e Assunto)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-$mail->Subject  = "Mensagem Teste"; // Assunto da mensagem
-$mail->Body = 'Este é o corpo da mensagem de teste, em HTML!';
-$mail->AltBody = 'Este é o corpo da mensagem de teste, em Texto Plano! \r\n';
-
-// Define os anexos (opcional)
-// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-//$mail->AddAttachment("/home/login/documento.pdf", "novo_nome.pdf");  // Insere um anexo
-
-// Envia o e-mail
-$enviado = $mail->Send();
-
-// Limpa os destinatários e os anexos
-$mail->ClearAllRecipients();
-$mail->ClearAttachments();
-
-// Exibe uma mensagem de resultado
-if ($enviado) {
-    echo "E-mail enviado com sucesso!";
-} else {
-    echo "Não foi possível enviar o e-mail.
-
-";
-    echo "Informações do erro:
-" . $mail->ErrorInfo;
-=======
 if(isset($_POST)){
 
 	$name = utf8_decode($_POST['name']);
@@ -138,5 +77,4 @@ if(isset($_POST)){
 	    echo "Mensagem enviada com sucesso|2";
 	}
 	die;
->>>>>>> de93bab439283f9df54e1d1a58d6622b7d32b961
 }
