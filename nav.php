a2e4310dd0233de4a6dc266ce85637a1ee6e31ea
unknown <fernando.seguim@NTSPPJ088.automatos.corp>
2014-10-26 23:42:44 -0200

    mudança no link do logo

--- nav.php
+++ nav.php
@@ -10,7 +10,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><img src="images/logo.png" width="112" height="51" /></a>
+            <a class="navbar-brand" href="index.php"><img src="images/logo.png" width="112" height="51" /></a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="navbarCollapse">
